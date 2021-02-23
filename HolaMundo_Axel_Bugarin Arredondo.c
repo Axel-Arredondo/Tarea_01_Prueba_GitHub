3dff4d6b9e5baf77f8c5549bc53bdee7e11564ae
Axel-Arredondo <79548148+Axel-Arredondo@users.noreply.github.com>
2021-02-23 17:53:27 -0600

    Add files via upload

---
+++ HolaMundo_Axel_Bugarin Arredondo.c
@@ -0,0 +1,9 @@
+//2AV4 Bugarin Arredondo Axel
+//HolaMundo_
+#include<stdio.h>
+int main()
+{
+    printf("\n-_-_-_-_-_-_ Hola Axel Bugarin Arredondo =)_-_-_-_-_-_--_-_-\n\n\n");
+
+    return 0;
+}
